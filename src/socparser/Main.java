5c2ab4be9073b1e79675f57e320f422f3b7f5670
John Manos <john.manos@miamidade.gov>
2019-10-24 03:33:10 -0400

    Implement TableView data display. Changed pane from StackPane to GridPane. Basic layout.

--- src/socparser/Main.java
+++ src/socparser/Main.java
@@ -3,6 +3,9 @@ package socparser;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -41,14 +44,24 @@ import java.util.logging.Logger;
 
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 
 
@@ -56,6 +69,10 @@ public class Main extends Application {
 	private Desktop desktop = Desktop.getDesktop();
 	private String fileName = null;
 	private final String dbUrl = "jdbc:mysql://localhost:3306/";
+    private ObservableList<ObservableList<String>> data = FXCollections.observableArrayList();
+    private Connection conn;
+    private TableView table = new TableView();
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -66,7 +83,7 @@ public class Main extends Application {
 	}	
 	private void createFileInputStage(Stage stage) {
 		stage.setTitle("Enter filename");
-		StackPane root = new StackPane();
+		GridPane root = new GridPane();
 		//TextField fileInput = new TextField();
 		final FileChooser fileChooser = new FileChooser();
         final Button openButton = new Button("Open a Spreadsheet");
@@ -83,9 +100,14 @@ public class Main extends Application {
 			}
 		};
 		openButton.setOnAction(event);
+		
 		//fileInput.setOnAction(event);
 		//root.getChildren().add(fileInput);
-		root.getChildren().add(openButton);
+		populateTable();
+		GridPane.setConstraints(openButton, 0, 1);
+		GridPane.setConstraints(table, 0, 2);
+		GridPane.setVgrow(table, Priority.ALWAYS);
+		root.getChildren().addAll(openButton, table);
 		Scene scene = new Scene(root, 600, 600);
 		scene.getStylesheets().add(getClass().getResource("socparser.css").toExternalForm());
 		stage.setScene(scene);
@@ -100,6 +122,46 @@ public class Main extends Application {
                 Main.class.getName()).log(
                     Level.SEVERE, null, ex
                 );
+        }
+    }
+	
+	private void populateTable(){
+		Connection c ;
+        try{
+        	conn = DriverManager.getConnection(dbUrl + "Courses", "root", "8675309aA^");
+        	String sql = "SELECT * from Sample";
+        	ResultSet rs = conn.createStatement().executeQuery(sql);
+		
+        	// Create columns based on sql table and add to tableView
+        	for(int i=0 ; i<rs.getMetaData().getColumnCount(); i++){
+        		final int j = i; // The factory below requires a final (or effectively final) variable to be used
+        		TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i+1));
+        		// Since we are retrieving data dynamically from a sql table, we cannot use PropertyValueFactory, so we use a Callback.
+        		// All cell value factories take a CellDataFeatures instance and return an ObservableValue.
+        		// Basically this stuff is stupid complicated so just follow the documentation and/or stackoverflow.
+        		// Filling a table using observables never looks clean but man does JavaFX take the cake.
+        		col.setCellValueFactory(new Callback<CellDataFeatures<ObservableList,String>, ObservableValue<String>>(){
+        			public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {
+        				return new SimpleStringProperty(param.getValue().get(j).toString());
+        			}
+        		});		
+        		table.getColumns().addAll(col); 
+        		//System.out.println("Column [" + i +"] ");
+        	}
+
+        	// Gather data into ObservableList to add to populate table
+        	while(rs.next()){
+        		ObservableList<String> row = FXCollections.observableArrayList();
+        		for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
+        			row.add(rs.getString(i));
+        		}
+        		//System.out.println("Row added " + row );
+        		data.add(row);
+        	}
+        	table.setItems(data);
+        }catch (Exception e) {
+        	e.printStackTrace();
+        	System.out.println("Error populating table.");             
         }
     }
 	
